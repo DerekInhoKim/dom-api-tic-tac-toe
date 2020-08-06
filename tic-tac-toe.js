74fd6b6ad8d46a9f75eb2fe83caa8173ec76aa00
mark <markmcclatchy@gmail.com>
2020-08-06 19:28:09 -0400

    End of Day

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,13 +1,12 @@
 window.addEventListener('DOMContentLoaded', event => {
-    const nextTurn = turnOrder()
-
-    const board = document.getElementById("tic-tac-toe-board")
-    board.addEventListener("click", event => {
-        const square = event.target.id
-        console.log("square " + square)
-        nextTurn(square)
-
-    })
+    
+    const resetButton = document.querySelectorAll('.reset');
+    resetButton.forEach(button => {
+        button.addEventListener('click', event => {
+            console.log("it worked...")
+            startGameBoard()
+        })    
+    })    
 
 
 
@@ -15,40 +14,49 @@ window.addEventListener('DOMContentLoaded', event => {
 
 })
 
-
-
-
-
+const startGameBoard = () => {
+    for (let i = 0; i < 9; i++) {
+        const gridId = `square-${i}`;
+        localStorage.setItem(gridId, '');
+    }
+    localStorage.setItem('X', 'https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-x.svg');
+    localStorage.setItem('O', 'https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-o.svg');
+}
 
 const turnOrder = () => {
-    let turnNum = 0;
+    let turn = 'X';
 
-    return (target) => {
-        turnNum++;
+    if (turn === 'X') {
 
-        if (turnNum % 2 === 0) {
-            let oImg = document.createElement('img');
-            oImg.setAttribute('src', 'https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-o.svg');
-            oImg.setAttribute('class', 'O');
-            target.innerHTML = oImg
+        const X = localStorage.getItem('X');
+        // `<img src="${X}"/>`
+        turn = 'O';
+    } else {
 
-        } else {
-
-            let xImg = document.createElement('img');
-            xImg.setAttribute('src', 'https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-x.svg');
-            xImg.setAttribute('class', 'X');
-            target.innerHTML = xImg
-        }
+        turn = 'X';
     }
 }
 
 
+
+
 /**********************************************************************************************
  *
+ *1) Object IS THE localStorage
+        -keys = grid-ID
+        -values = X || O
+ *2) Establish the winning patterns
+        -Switch / Case 
+        -addEventListener
+            -Function that is invoked each click to verify possible winning combos
+ *3) In order to start a game, the user MUST CLICK "New Game" Button
+ *4) Function: (on click)
+ *      -Verify if grid is available (proceed) or already been taken (return )
+ *      -Pass <img> to associated grid ---->  Based on Obj Value stored in localStorage
+ *      -Pass value into Obj
+ *      -Invoke winning cases Function
  *
- *
- *
- *
- *
- *
+ * 5) New Game  ---->  Clear localStorage   ---> HIDDEN while in play
+ * 6) Give Up   ---->  Mock the user and clear localStorage   ----> SHOWS while in play & if pushed other player wins
+ * 7) Bonus Computer Mode
  ***********************************************************************************************/
