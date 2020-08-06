89a3e38fce8050686a838b986c311047e5a01fcf
Derek <derekkiminho@gmail.com>
2020-08-06 14:18:39 -0700

    bugfixing 'X' and 'O'

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,6 +1,13 @@
 window.addEventListener('DOMContentLoaded', event => {
+    const nextTurn = turnOrder()
 
+    const board = document.getElementById("tic-tac-toe-board")
+    board.addEventListener("click", event => {
+        const square = event.target.id
+        console.log("square " + square)
+        nextTurn(square)
 
+    })
 
 
 
@@ -16,30 +23,32 @@ window.addEventListener('DOMContentLoaded', event => {
 const turnOrder = () => {
     let turnNum = 0;
 
-    return () => {
+    return (target) => {
         turnNum++;
-        
+
         if (turnNum % 2 === 0) {
             let oImg = document.createElement('img');
             oImg.setAttribute('src', 'https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-o.svg');
             oImg.setAttribute('class', 'O');
-            
+            target.innerHTML = oImg
+
         } else {
-            
+
             let xImg = document.createElement('img');
             xImg.setAttribute('src', 'https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-x.svg');
             xImg.setAttribute('class', 'X');
+            target.innerHTML = xImg
         }
     }
 }
 
 
 /**********************************************************************************************
- * 
- * 
- * 
- * 
- * 
- * 
- * 
+ *
+ *
+ *
+ *
+ *
+ *
+ *
  ***********************************************************************************************/
